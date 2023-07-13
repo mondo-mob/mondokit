484789c8c4f7b9713e03e5a4bed1764684c5c5ca
Rob Fyfe <rob@mondo.ventures>
2023-07-13 21:07:38 +1000

    Update eslint to cjs extension for ESM import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  ignorePatterns: [".eslintrc.js", "jest.config.js", "jest-*.js", "**/dist/**"],
+  ignorePatterns: [".eslintrc.cjs", "jest.config.js", "jest-*.js", "**/dist/**"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
