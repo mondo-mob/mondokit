486a241b16eea60eb2b888b6b3e3bb9c83619d1d
Matt Byrne <mbyrne00@users.noreply.github.com>
2023-12-15 10:44:32 +1100

    Remove deprecated asArray function

---
+++ .changeset/hungry-doors-tap.md
@@ -0,0 +1,5 @@
+---
+"@mondokit/core": patch
+---
+
+Remove deprecated asArray function
--- packages/core/src/util/types.ts
+++ packages/core/src/util/types.ts
@@ -1,8 +1,3 @@
-import { castArray } from "lodash-es";
-
 export type OneOrMany<T> = T | ReadonlyArray<T>;
-
-/** @deprecated use lodash castArray instead */
-export const asArray = <T>(input: OneOrMany<T>): T[] => castArray(input);
 
 export const isReadonlyArray = <T>(input: OneOrMany<T>): input is ReadonlyArray<T> => Array.isArray(input);
